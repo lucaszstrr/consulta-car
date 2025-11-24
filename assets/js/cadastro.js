7972e761223c9c7802283de54d79e41776ea1b3b
Lucas <lucaaszstr@gmail.com>
2025-11-24 12:08:44 -0300

    validacao de cep funcionando

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -2,3 +2,77 @@
 $(document).ready(function() {
     $('#cep').mask('00000-000');
 });
+
+//Cadastro
+document.getElementById('formCadastro').addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const nome = document.getElementById('nome').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const senha = document.getElementById('senha').value.trim();
+    const confirmarSenha = document.getElementById('confirmarSenha').value.trim();
+    const cep = document.getElementById('cep').value.trim();
+
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    let resultadoCEP = true;
+
+    fetch(url).then(res => res.json()).then(data => {
+        
+        if (data.erro) {
+            mensagem = "CEP inválido!";
+            resultadoCEP = false;
+        } else {
+            resultadoCEP = true;
+        }
+
+        const users = JSON.parse(localStorage.getItem("users")) || [];
+        const userExists = users.find(user => user.email === email);
+
+        if (userExists) {
+
+            mensagem = "Este email já foi cadastrado!";
+        
+        } else if (senha != confirmarSenha) {
+        
+            mensagem = "As senhas não coincidem!";
+        
+        } else if (!resultadoCEP) {
+        
+            mensagem = "CEP inválido!";
+        
+        } else {
+
+            users.push({ nome, email, senha, cep });
+            localStorage.setItem("users", JSON.stringify(users));
+            mensagem = "Cadastro realizado com sucesso!";
+            redirect = true;
+
+        }
+
+        // Exibicao modal
+        const msgTexto = document.getElementById("msgTexto");
+        msgTexto.textContent = mensagem;
+
+        const modal = new bootstrap.Modal(document.getElementById("mensagemModal"));
+        modal.show();
+
+        // Redirecionamento se o cadastro deu certo
+        const modalElement = document.getElementById("mensagemModal");
+        modalElement.addEventListener("hidden.bs.modal", function () {
+            if (redirect) {
+                window.location.href = "login.html";
+            }
+        }, { once: true });
+    
+    }).catch(() => {
+
+    mensagem = "Erro ao verificar CEP.";
+    const msgTexto = document.getElementById("msgTexto");
+    msgTexto.textContent = mensagem;
+
+    const modal = new bootstrap.Modal(document.getElementById("mensagemModal"));
+    modal.show();
+ 
+    });
+});
