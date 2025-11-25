9e36c594653cb41e505f9f3772d4054d955e7d1c
Lucas <lucaaszstr@gmail.com>
2025-11-25 11:49:35 -0300

    arrumados os dados do form de envio

--- assets/js/cadastroveiculo.js
+++ assets/js/cadastroveiculo.js
@@ -1,8 +1,8 @@
 //Mascara para o campo de placa aceitar os dois tipos, mercosul e padrao
 $(document).ready( function(){
-    $('#placa').mask('AAA0S00', {
+    $('#placa').mask('AAA0B00', {
         translation: {
-            'S': { pattern: /[A-Za-z\d/]/ }
+            'B': { pattern: /[A-Za-z\d/]/ }
         }
     });
 });
@@ -11,6 +11,8 @@ const tipoSelect = document.getElementById("tipo");
 const marcaSelect = document.getElementById("marca");
 const modeloSelect = document.getElementById("modelo");
 const anoSelect = document.getElementById("ano");
+
+let tipoString = "";
 
 tipoSelect.addEventListener("change", async () => {
     const tipo = tipoSelect.value;
@@ -28,9 +30,15 @@ tipoSelect.addEventListener("change", async () => {
 
     marcaSelect.innerHTML = '<option value="">Selecione a marca</option>';
     marcas.forEach(m => {
-        marcaSelect.innerHTML += `<option value="${m.codigo}">${(m.nome).toUpperCase()}</option>`;
+        marcaSelect.innerHTML += `<option value="${m.codigo}" data-nome="${(m.nome).toUpperCase()}">${(m.nome).toUpperCase()}</option>`;
     });
+
+    if (tipo == 'carros') tipoString = 'Carro';
+    if (tipo == 'motos') tipoString = 'Moto';
+    if (tipo == 'caminhoes') tipoString = 'Caminhão';
+
     marcaSelect.disabled = false;
+    marcaSelect.required = true;
 });
 
 marcaSelect.addEventListener("change", async () => {
@@ -44,9 +52,11 @@ marcaSelect.addEventListener("change", async () => {
 
     modeloSelect.innerHTML = '<option value="">Selecione o modelo</option>';
     data.modelos.forEach(m => {
-        modeloSelect.innerHTML += `<option value="${m.codigo}">${(m.nome).toUpperCase()}</option>`;
+        modeloSelect.innerHTML += `<option value="${m.codigo}" data-nome="${(m.nome).toUpperCase()}">${(m.nome).toUpperCase()}</option>`;
     });
+
     modeloSelect.disabled = false;
+    modeloSelect.required = true;
 });
 
 modeloSelect.addEventListener("change", async () => {
@@ -63,26 +73,23 @@ modeloSelect.addEventListener("change", async () => {
     anos.forEach(a => {
         anoSelect.innerHTML += `<option value="${a.codigo}">${a.nome}</option>`;
     });
+
     anoSelect.disabled = false;
+    anoSelect.required = true;
 });
 
 document.getElementById('formVeiculo').addEventListener('submit', function(e){
     e.preventDefault();
 
-    const tipo = document.getElementById('tipo').value.trim();
-    const marca = document.getElementById('marca').value.trim();
-    const modelo = document.getElementById('modelo').value.trim();
-    const ano = document.getElementById('ano').value.trim();
-    const placa = document.getElementById('placa').value.trim();
+    const marca = marcaSelect.options[marcaSelect.selectedIndex].dataset.nome;
+    const modelo = modeloSelect.options[modeloSelect.selectedIndex].dataset.nome;
+    const ano = anoSelect.value;
+    const placa = document.getElementById('placa').value.trim().toUpperCase();
 
-    // const tipo = tipoSelect.value;
-    // const marca = marcaSelect.value;
-    // const modelo = modeloSelect.value;
-    // const ano = anoSelect.value;
-
-    console.log(tipo);
+    console.log(tipoString);
     console.log(marca);
     console.log(modelo);
     console.log(ano);
+    console.log(placa);
 
 });
